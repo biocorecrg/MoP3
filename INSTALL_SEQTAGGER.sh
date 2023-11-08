1c530c6667a8734d255d0e8e19a46cc10ecae811
Luca Cozzuto <lucacozzuto@gmail.com>
2023-11-08 16:41:29 +0100

    add INSTALL SEQTAGGET

---
+++ INSTALL_SEQTAGGER.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This script will download and install seqtagger
+# params:
+# 1 user
+# 2 password
+
+if [ x"$1" == x ]; then
+        echo "please enter username"
+	exit
+fi
+if [ x"$2" == x ]; then
+        echo "please enter password"
+        exit
+fi
+
+wget $1:$2@https://public-docs.crg.es/biocore/projects/mop3/SeqTagger.tar.gz
+tar -zvxf SeqTagger.tar.gz
+mv SeqTagger/* mop_preprocess/bin/
+rm SeqTagger.tar.gz
+
