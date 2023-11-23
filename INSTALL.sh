08bf26a8737cb6144b2420269e2219af77390912
Luca Cozzuto <lucacozzuto@gmail.com>
2023-11-23 13:44:59 +0100

    INSTALL now keep the version

--- INSTALL.sh
+++ INSTALL.sh
@@ -21,16 +21,23 @@ fi
 
 tar -zvxf ont-guppy_${GUPPY_VER}_linux64.tar.gz
 for i in custom_models/*.gz; do gzip -cd $i > ont-guppy/data/`basename $i .gz`; done
-mv ont-guppy mop_preprocess/bin/
+mkdir -p ./mop_preprocess/bin/ont-guppy_${GUPPY_VER}
+mv ont-guppy/* ./mop_preprocess/bin/ont-guppy_${GUPPY_VER}
+rmdir ont-guppy
 
-cd mop_preprocess/bin
-ln -s ont-guppy/bin/guppy_* .
-ln -s ont-guppy/lib/* .
+cd ./mop_preprocess/bin/
+ln -s ont-guppy_${GUPPY_VER}/bin/guppy_* .
+ln -s ont-guppy_${GUPPY_VER}/lib/* .
 cd ../../
-if [ ! -e "mop_preprocess/bin/ont-guppy/lib/libz.so" ] ; then
-        unlink mop_preprocess/bin/ont-guppy/lib/libz.so
-        cd mop_preprocess/bin/ont-guppy/lib/
-        ln -s libz.so.1 libz.so
-        cd ../../../../
+
+if [ ! -e "./mop_preprocess/bin/ont-guppy_${GUPPY_VER}/lib/libz.so" ] ; then
+	if [ -e "./mop_preprocess/bin/ont-guppy_${GUPPY_VER}/lib/libz.so.1" ] ; then
+        	unlink mop_preprocess/bin/ont-guppy_${GUPPY_VER}/lib/libz.so
+        	cd mop_preprocess/bin/ont-guppy_${GUPPY_VER}/lib/
+        	ln -s libz.so.1 libz.so
+        	cd ../../../../
+	fi
 fi
+
 rm ont-guppy_${GUPPY_VER}_linux64.tar.gz
+
