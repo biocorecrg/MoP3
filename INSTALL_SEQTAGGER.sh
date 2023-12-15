977056a559051f4f80852f2209efe230c5c8e538
Luca Cozzuto <lucacozzuto@gmail.com>
2023-12-15 16:52:26 +0100

    adding demux data

--- INSTALL_SEQTAGGER.sh
+++ INSTALL_SEQTAGGER.sh
@@ -14,7 +14,7 @@ if [ x"$2" == x ]; then
         exit
 fi
 
-wget --user $1 --password $2 https://public-docs.crg.es/biocore/projects/mop3/SeqTagger.tar.gz
+wget --user $1 --password $2 https://public-docs.crg.es/biocore/projects/mop3/seqtagger/SeqTagger.tar.gz
 tar -zvxf SeqTagger.tar.gz
 mv SeqTagger/* mop_preprocess/bin/
 rm SeqTagger.tar.gz
