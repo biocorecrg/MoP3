60b19ee21ed0f236edb9d69c06b653966dd90377
Luca Cozzuto <lucacozzuto@gmail.com>
2024-01-11 16:53:13 +0100

    changing webserver

--- INSTALL_SEQTAGGER.sh
+++ INSTALL_SEQTAGGER.sh
@@ -14,8 +14,8 @@ if [ x"$2" == x ]; then
         exit
 fi
 
-wget --user $1 --password $2 https://public-docs.crg.es/biocore/projects/mop3/seqtagger/SeqTagger.tar.gz
+wget --user $1 --password $2 https://biocore.crg.eu/public/mop3/seqtagger/SeqTagger.tar.gz
 tar -zvxf SeqTagger.tar.gz
 mv SeqTagger/* mop_preprocess/bin/
 rm SeqTagger.tar.gz
-
+rm -fr SeqTagger
