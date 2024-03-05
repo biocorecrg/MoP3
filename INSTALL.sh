bddd08236c21a46a85d2f988bb85220e7ac257fa
Luca Cozzuto <lucacozzuto@gmail.com>
2024-03-05 12:30:15 +0100

    improving INSTALL

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,6 +4,11 @@
 # params:
 # 1 guppy version
 
+if [ -e "./mop_preprocess/bin/guppy_basecaller" ] ; then
+	echo "unlinking previously installed versions"
+	cd mop_preprocess/bin; find . -maxdepth 1 -type l | xargs rm; cd ../../
+fi
+
 if [ x"$1" == x ]; then
         GUPPY_VER='3.4.5'
 else
@@ -11,32 +16,26 @@ else
 fi
 
 
-wget https://cdn.oxfordnanoportal.com/software/analysis/ont-guppy_${GUPPY_VER}_linux64.tar.gz 
-if [ $? -eq 0 ]; then
-	echo "INSTALLING GUPPY VERSION ${GUPPY_VER}"
-else
-    echo "GUPPY VERSION ${GUPPY_VER} is not found" 
-    exit
-fi
+#wget https://cdn.oxfordnanoportal.com/software/analysis/ont-guppy_${GUPPY_VER}_linux64.tar.gz 
+#if [ $? -eq 0 ]; then
+#	echo "INSTALLING GUPPY VERSION ${GUPPY_VER}"
+#else
+#    echo "GUPPY VERSION ${GUPPY_VER} is not found" 
+#    exit
+#fi
 
 tar -zvxf ont-guppy_${GUPPY_VER}_linux64.tar.gz
 
 wget https://biocore.crg.eu/public/mop3_pub/models.tar
-mv models.tar custom_models/
-cd custom_models; tar -xvf  models.tar; rm models.tar; cd ../
+mv models.tar mop_preprocess/guppy_models/
+cd mop_preprocess/guppy_models; tar -xvf  models.tar; rm models.tar; cd ../../
 
-for i in custom_models/*.gz; do gzip -cd $i > ont-guppy/data/`basename $i .gz`; done
-mkdir -p ./mop_preprocess/bin/ont-guppy_${GUPPY_VER}
-mv ont-guppy/* ./mop_preprocess/bin/ont-guppy_${GUPPY_VER}
+mkdir -p mop_preprocess/bin/ont-guppy_${GUPPY_VER}
+mv ont-guppy/* mop_preprocess/bin/ont-guppy_${GUPPY_VER}
+for i in mop_preprocess/guppy_models/*.gz; do gzip -cd $i > mop_preprocess/bin/ont-guppy_${GUPPY_VER}/data/`basename $i .gz`; done
 rmdir ont-guppy
 
-if [ -e "./mop_preprocess/bin/guppy_basecaller" ] ; then
-	rm ./mop_preprocess/bin/guppy_*; rm ./mop_preprocess/bin/lib*
-fi
-if [ -e "./mop_preprocess/bin/MINIMAP2_LICENSE" ] ; then
-        rm ./mop_preprocess/bin/MINIMAP*;
-fi
-cd ./mop_preprocess/bin/
+cd mop_preprocess/bin
 ln -s ont-guppy_${GUPPY_VER}/bin/guppy_* .
 ln -s ont-guppy_${GUPPY_VER}/lib/* .
 cd ../../
@@ -50,5 +49,4 @@ if [ ! -e "./mop_preprocess/bin/ont-guppy_${GUPPY_VER}/lib/libz.so" ] ; then
 	fi
 fi
 
-rm ont-guppy_${GUPPY_VER}_linux64.tar.gz
-
+rm ./ont-guppy_${GUPPY_VER}_linux64.tar.gz
